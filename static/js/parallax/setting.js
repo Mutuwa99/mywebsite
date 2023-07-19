b51fc829c9627bfbd3c2be3089bccb616d68c39e
Mutuwa99 <isayamulaudzi@gmail.com>
2023-07-19 10:57:54 +0200

    back to static

--- static/js/parallax/setting.js
+++
@@ -1,8 +0,0 @@
-var parallax = function() {
-    'use strict';
-
-    $('.parallax').parallax("50%", 0, 0.1, true);
-    $('.parallax-two').parallax("50%", 0, 0.1, true);
-    $('.parallax-three').parallax("50%", 0, 0.1, true);
-    $('.parallax-four').parallax("50%", 0, 0.1, true);
-}();
